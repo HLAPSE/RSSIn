b79fe36c438449d24087bf7d823f77226cfe6655
hlapse <root@hlapse.top>
2021-04-14 23:41:36 +0800

    修复小错误

--- demo/src/views/Note.vue
+++ demo/src/views/Note.vue
@@ -321,7 +321,7 @@ export default {
       console.log(index, row);
     };
     const handleDelete = (index, row) => {
-      if (！row.note_count) {
+      if (!row.note_count) {
         ElMessage.error("文件夹不为空");
       } else {
         ctx.$axios
